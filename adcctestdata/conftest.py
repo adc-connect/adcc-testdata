2a3cad515b160bf7fddc5a5e6dffb8769dcfd2e6
Michael F. Herbst <info@michael-herbst.com>
2019-10-06 20:03:43 +0200

    Add conftest for Travis-CI

---
+++ adcctestdata/conftest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2019 by Michael F. Herbst
+##
+## This file is part of adcc-testdata.
+##
+## adcc-testdata is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## adcc-testdata is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with adcc-testdata. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+import os
+
+
+def pytest_runtestloop(session):
+    import pyadcman
+
+    # Reduce threads in Travis session
+    if "TRAVIS" in os.environ or "CI" in os.environ:
+        pyadcman.thread_pool.reinit(2, 3)
